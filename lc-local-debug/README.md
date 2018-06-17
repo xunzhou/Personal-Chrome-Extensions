7b025351b9649a6c79e29f26873e74228cf3fa45
xunzhou <xzh.dev@gmail.com>
2018-06-16 23:57:55 -0700

    first init

---
+++ lc-local-debug/README.md
@@ -0,0 +1,5 @@
+# Local-LC-Debug-ChromeExt
+Chrome Extension for personal use.  
+
+- Generate line of shell script to launch editor with formatted filename.
+- Click Extension Icon, copy generated line to clipboard. 
---
+++ lc-local-debug/genName.js
@@ -0,0 +1,27 @@
+var editor = "code ";
+var path = "/home/user/practice/LeetCode/"
+var file = null;
+var ext = ".java";
+var res = null;
+
+function copyToClipboard(text){
+    var dummy = document.createElement("input");
+    document.body.appendChild(dummy);
+    dummy.setAttribute('value', text);
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+}
+
+function getProb(document) {
+    var prob = document.getElementsByTagName("h3")[0].innerHTML.split(".");
+    file = "LC"+prob[0]+"_"+prob[1].trim().replace(/ /g, "_");
+    res = editor+path+file+ext;
+    copyToClipboard(res);
+    return res;
+}
+
+chrome.runtime.sendMessage({
+    action: "getSource",
+    source: getProb(document)
+});
